894aa51017df577e73b64e513c5fcb5f875b2971
mortezasohrabi <mortezasohrabi232@gmail.com>
2024-10-31 11:15:17 +0330

    Incom model

--- w/models.py
+++ w/models.py
@@ -8,4 +8,11 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User , on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.user} {self.text } "
+class Income(models.Model):
+    text = models.CharField(max_length=300)
+    date = models.DateField(auto_now_add=True)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
 
