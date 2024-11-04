afc2b6369de847f03d56114ca7bcd54fdd1729d9
mortezasohrabi <mortezasohrabi232@gmail.com>
2024-11-04 11:53:33 +0330

    token model

--- w/models.py
+++ w/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Token(models.Model):
+    user=models.OneToOneField(User , on_delete = models.CASCADE)
+    token=models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user}_token"
+
 class Expense(models.Model):
     text = models.CharField(max_length=300)
     date = models.DateField(auto_now=True)
--- w/views.py
+++ w/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from .models import Expense , User ,Token , Income
 # Create your views here.
+
+
+@csrf_exempt
+def submit_expense(request):
+
+    this_token=request.POST['token']
+    this_user=User.objects.filter(token__token=this_token).get()
+    Expense.objects.create(user=this_user , amount=request.POST['amount'] , text=request.POST['text'] , date=request.POST['date'])
+
+    return JsonResponse({
+        'statuse':'ok'
+    } , encoder=JSONEncoder)
